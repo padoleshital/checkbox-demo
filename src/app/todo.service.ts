aac9178c50cbbf85474c1c7374e31216739ed504
Shital padole <45750512+padoleshital@users.noreply.github.com>
2023-11-01 10:29:18 +0530

    Update todo.service.ts

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -6,38 +6,47 @@ import { map } from 'rxjs';
   providedIn: 'root'
 })
 export class TodoService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTodo() {
-    return this.http.get<any>(`${environment.baseUrl}/todos`)
-      .pipe(map((res: any) => {
-        return res;
-      }))
-
+    return this.http
+      .get<any>(`https://jsonplaceholder.typicode.com/todos`)
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
   }
 
   createTodo(data: any) {
-    return this.http.post<any>(`${environment.baseUrl}/todos`, data)
-      .pipe(map((res: any) => {
-        return res;
-      }))
-
+    // return this.http.post<any>(`${environment.baseUrl}/todos`, data)
+    return this.http
+      .post<any>(`https://jsonplaceholder.typicode.com/todos`, data)
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
   }
 
-  updateTodoStatus(task: any, id: any) {
-    return this.http.put(`${environment.baseUrl}/todos/${id}`, task)
-      .pipe(map((res: any) => {
-        return res;
-      }))
-
+  updateTodoStatus(data: any, id: any) {
+    // return this.http.put(`${environment.baseUrl}/todos/${id}`, task)
+    return this.http
+      .put(`https://jsonplaceholder.typicode.com/todos/${id}`, data)
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
   }
   DeleteTodotask(id: any) {
-    return this.http.delete(`${environment.baseUrl}/todos/${id}`, id)
-      .pipe(map((res: any) => {
-        return res;
-      }))
-
+    // return this.http.delete(`${environment.baseUrl}/todos/${id}`, id)
+    return this.http
+      .put(`https://jsonplaceholder.typicode.com/todos/${id}`, id)
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
   }
-
 }
